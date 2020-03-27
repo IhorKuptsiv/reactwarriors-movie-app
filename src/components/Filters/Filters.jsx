75afb5c263fa169318faa75277e2df8c4cc87633
Ihor Kuptsiv <cronosgothic@gmail.com>
2020-03-27 11:32:25 +0200

    add SortBy.jsx

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,41 +1,20 @@
 import React from "react";
+import SortBy from "./SortBy";
+
 
 export default class Filters extends React.Component {
   render() {
-    const {filters: {sort_by,primary_release_year},
-    page,
-    onChangeFilters,
-    onChangePage
-  
-  } = this.props;
+    const {
+      filters: { sort_by},
+      page,
+      onChangeFilters,
+      onChangePage
+    } = this.props;
     return (
       <form className="mb-3">
-        <div className="form-group">
-          <label htmlFor="sort_by">Сортировать по:</label>
-          <select className="form-control" 
-          id="sort_by"
-          value={sort_by} 
-          name="sort_by"
-          onChange={onChangeFilters}>
-            <option value="popularity.desc">Популярные по убыванию</option>
-            <option value="popularity.asc">Популярные по возростанию</option>
-            <option value="vote_average.desc">Рейтинг по убыванию</option>
-            <option value="vote_average.asc">Рейтинг по возростанию</option>
-          </select>
-        </div>
-
-                                      {/*   primary_release_year */}
-        <div className="form-group">
-        <label htmlFor="primary_release_year">Сортировать по году:</label>
-        <select className="form-control" 
-        id="primary_release_year"
-        value={primary_release_year} 
-        name="primary_release_year"
-        onChange={onChangeFilters}>
-          <option value="primary_release_year">Выберите год</option>
-          
-        </select>
-      </div>
+   
+        <SortBy sort_by={sort_by} onChangeFilters={onChangeFilters} />
+       
 
 
         <div className="btn-group">
