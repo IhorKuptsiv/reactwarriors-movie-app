4848861aa8d4b8c33986f7578cd765f6f41a237d
Ihor Kuptsiv <cronosgothic@gmail.com>
2020-03-31 11:53:53 +0300

    add Pagination

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -8,16 +8,20 @@ export default class Filters extends React.Component {
       filters: { sort_by, primary_release_year},
       page,
       onChangeFilters,
-      onChangePage
+      onChangePage,
+      onChangeReset
     } = this.props;
     return (
       <form className="mb-3">
+      <div>
+      <button type="button" className="btn btn-light" onClick={onChangeReset}>
+        Сбросить фильтры
+      </button>
+    </div>
    
         <SortBy sort_by={sort_by} onChangeFilters={onChangeFilters} />
         <ReleaseYear primary_release_year={primary_release_year} onChangeFilters={onChangeFilters} />
        
-
-
         <div className="btn-group">
         <button
           type="button"
@@ -34,8 +38,6 @@ export default class Filters extends React.Component {
         >
           Вперед
         </button>
-
-  
 
         
       </div>
---
+++ src/components/Filters/Pagination.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class Pagination extends React.PureComponent {
+  static propTypes = {
+    page: PropTypes.number.isRequired,
+    total_pages: PropTypes.number.isRequired,
+    onChangePagination: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { page, total_pages, onChangePagination } = this.props;
+    return (
+      <div>
+        <div>
+          <button
+            type="button"
+            className="btn mr-2"
+            disabled={page === 1}
+            onClick={onChangePagination.bind(null, page - 1, total_pages)}
+          >
+            Назад
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={onChangePagination.bind(null, page + 1, total_pages)}
+          >
+            Вперед
+          </button>
+        </div>
+        <div className="page-pagination">
+          {page} из {total_pages}
+        </div>
+      </div>
+    );
+  }
+}
